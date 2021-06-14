6db00f9af935f732ec0212071d8fd0af5276351d
jaydeep rajput <jaydeep1998rajput@gmail.com>
2021-06-14 13:14:38 +0530

    "Second COmmit"

--- src/main/java/com/jaydeep/restservices/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/jaydeep/restservices/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
-
+	//2 commit
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
 	}
